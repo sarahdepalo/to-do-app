a1a9224509e2b962c5356b5c353cf41a72c19818
Nick Aldridge <naldridge1360@gmail.com>
2021-07-01 13:38:23 -0500

    added files

---
+++ sql/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO users
+    (name, username, password)
+VALUES
+('Oswald', 'penguin', 'password'),
+('Barb', 'batgirl', 'password'),
+('Bane', 'bane', 'password');
+
+
+INSERT INTO tasks
+    (task_name, task_description, user_id)
+VALUES
+('Create Villain Group', 'Create supervillain group to take-down the Bat!', 1),
+('Foil Villains', 'Stop the baddies from completing their plan', 2);
